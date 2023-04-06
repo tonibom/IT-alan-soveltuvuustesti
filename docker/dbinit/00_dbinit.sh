f3543f4505f055dbfb716cfb39741d643f8a9ff5
Toni <Toni Bomstrm>
2023-04-06 10:46:34 +0300

    Refactor and extend database schema
    Add specific ID column to the study_programs table. Traditionally, the
    DBID column would be used for referencing between tables and this new
    column is a bit redundant, but seeing as the DBID is not as easily
    modifiable, and the frontend configuration depends on reliable ID
    numbering, it is likely a good idea to separate this from DBID.
    
    Rename the respondents table to survey_answers, and include more
    optional fields (as well as the mandatory page arrival timestamp).

--- docker/dbinit/00_dbinit.sh
+++ docker/dbinit/00_dbinit.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 set -e
 
-# Core elements of the database are configured here.
+# The database schema is defined here.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER $ROLE_NAME WITH PASSWORD '$ROLE_PASSWORD';
 	CREATE DATABASE $DB_NAME WITH OWNER = $ROLE_NAME;
 	GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $ROLE_NAME;
 	GRANT USAGE ON SCHEMA PUBLIC TO $ROLE_NAME;
 
-        -- Create new tables in the database as the created user.
+        -- Create new tables in the created database as the created user.
         \connect $DB_NAME
         SET ROLE $ROLE_NAME;
 
         CREATE TABLE study_programs (
             dbid BIGSERIAL UNIQUE NOT NULL,
+            id INT UNIQUE NOT NULL,
             name TEXT NOT NULL
-
         );
 
-        CREATE TABLE respondents (
+        CREATE TABLE survey_answers (
             dbid BIGSERIAL UNIQUE NOT NULL,
             consent_given BOOL NOT NULL DEFAULT FALSE,
+            page_loaded TIMESTAMP WITH TIME ZONE NOT NULL,
+            started TIMESTAMP WITH TIME ZONE,
+            completed TIMESTAMP WITH TIME ZONE,
+            locale TEXT,
             email BYTEA,
-            survey_time INT,
             answers JSON,
-            result INT REFERENCES study_programs(dbid)
+            result INT REFERENCES study_programs(id)
         );
 EOSQL
 
