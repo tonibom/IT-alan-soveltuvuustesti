df5793b9d94d2b944bc5f34ec6b22d1d97442548
Toni <Toni Bomstrm>
2023-04-06 12:26:19 +0300

    api/api: Store timestamp upon page load
    Store timestamp into database when the survey page is requested.

--- api/api.py
+++ api/api.py
@@ -1,4 +1,7 @@
+import datetime
 import os
+
+import psycopg2
 
 from flask import Flask
 from flask import jsonify
@@ -7,8 +10,27 @@ from flask import jsonify
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
+def get_dbcon():
+    dbcon = psycopg2.connect(host=os.environ['SQL_HOST'],
+                             database=os.environ['DB_NAME'],
+                             user=os.environ['ROLE_NAME'],
+                             password=os.environ['ROLE_PASSWORD'])
+    return dbcon
+
+
 @app.route('/')
 def index():
+    page_loaded_dt = datetime.datetime.now()
+
+    with get_dbcon() as dbcon:
+        with dbcon.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO
+                    survey_answers (page_loaded)
+                VALUES (%s)
+                ;""", (page_loaded_dt,))
+
     return app.send_static_file('index.html')
 
 
