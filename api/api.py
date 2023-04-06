cc0051b73cce1e02843ac31430a57b1242ad967b
Toni <Toni Bomstrm>
2023-04-06 18:14:23 +0300

    api/api: POST handler for survey completion
    Receive the survey results, check if consent is given to store answers,
    gotten result, and possible email address. Store these data only if
    consent is given.
    
    Remove language, consent, and email address questions and answers from
    the answers json before storing it.
    
    Regardless, reset session upon receiving this request (so that the user
    may redo the survey and store new results).

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os
 
 import psycopg2
@@ -106,6 +107,113 @@ def set_survey_start():
                     dbid = %s
                 ;""", (survey_start_dt, dbid))
             dbcon.commit()
+
+    response = jsonify(success=True)
+    return response
+
+
+@app.route('/survey-complete', methods=('POST',))
+def survey_complete():
+    if not session.get('answer-id'):
+        abort(400)
+
+    dbid = session['answer-id'][0]
+
+    results_json = request.json.copy()
+
+    # Survey completed; Reset the session cookie in case the user
+    # decides to reload the page and redo the survey.
+    session['answer-id'] = None
+
+    # Make sure each key has either a real or a None value.
+    results_json['data-collect-consent'] = results_json.get('data-collect-consent')
+    results_json['email'] = results_json.get('email')
+    results_json['answers'] = results_json.get('answers')
+    results_json['result'] = results_json.get('result')
+
+    if not results_json['data-collect-consent']:
+        # If consent is not given, make sure potential personal data is
+        # not even accidentally stored.
+        results_json['email'] = None
+        results_json['answers'] = None
+        results_json['result'] = None
+
+    if results_json['result'] is not None:
+        # The result is stored as an integer that references the study
+        # programs table.
+        results_json['result'] = int(results_json['result'])
+
+    locale = None
+    if results_json['answers'] is not None:
+        # Delete redundant fields from the answers.
+        if 'language_selection' in results_json['answers']:
+            # Grab the locale from the results: This is the language
+            # the survey was finished in. In case it happens to be
+            # different than the one in database, we update the row.
+            locale = results_json['answers']['language_selection']
+            del results_json['answers']['language_selection']
+
+        if 'data_collection_consent' in results_json['answers']:
+            del results_json['answers']['data_collection_consent']
+
+        if 'email_address' in results_json['answers']:
+            del results_json['answers']['email_address']
+
+        # If the answers are provided and we can store them, cast them
+        # to string. Casting None into "null" is not very useful.
+        results_json['answers'] = json.dumps(results_json['answers'])
+
+    # TODO: Encrypt email address.
+
+    with get_dbcon() as dbcon:
+        with dbcon.cursor() as cur:
+            cur.execute(
+            """
+            SELECT
+                completed IS NOT NULL
+            FROM
+                survey_answers
+            WHERE
+                dbid = %s
+            ;""", (dbid, )
+            )
+            already_answered = cur.fetchone()[0]
+            if already_answered:
+                # Technically this check is a bit redundant with the
+                # session dbid check, but it does not hurt to make sure.
+                abort(403)
+
+            survey_complete_dt = datetime.datetime.now()
+            params = (
+                results_json['data-collect-consent'],
+                survey_complete_dt,
+                locale,
+                results_json['email'],
+                results_json['answers'],
+                results_json['result'],
+                dbid,
+            )
+            try:
+                # Update locale only if the new value is not NULL (None)
+                # regardless of the previous value.
+                cur.execute(
+                    """
+                    UPDATE
+                        survey_answers
+                    SET
+                        consent_given = %s,
+                        completed = %s,
+                        locale = COALESCE(%s, locale),
+                        email = %s,
+                        answers = %s,
+                        result = %s
+                    WHERE
+                        dbid = %s
+                    ;""", params)
+                dbcon.commit()
+            except Exception as e:
+                print(e)
+                abort(400)
 
     response = jsonify(success=True)
     return response
